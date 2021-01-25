1c97dd25ad27fd4173e8136bc328bc949a322615
aracuans <75974459+aracuans@users.noreply.github.com>
2021-01-25 14:57:50 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+<p>JUST AS STUDY</p>
---
+++ src/help.js
@@ -0,0 +1,28 @@
+const help = (prefix) => {
+	return `
+*${prefix}randomquotes*
+*${prefix}ytmp3 link*
+*${prefix}ytmp4 link*
+*${prefix}tiktok link*
+*${prefix}sticker*
+*${prefix}toimg*
+*${prefix}meme*
+*${prefix}memeindo*
+*${prefix}gtts*
+*${prefix}simi*
+*${prefix}ocr*
+*${prefix}setprefix*
+walning : This setprefix command can only be used by the bot owner\n
+----[ *group only* ]----
+*${prefix}add*
+*${prefix}kick*
+*${prefix}promote*
+*${prefix}demote*
+*${prefix}linkgroup*
+*${prefix}leave*
+*${prefix}tagall*
+*${prefix}simih 1/0*
+`
+}
+
+exports.help = help
